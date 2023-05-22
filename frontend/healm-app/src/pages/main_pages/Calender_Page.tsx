94fcfcc945ef24262522e874b2c5ea55cf8e3c67
Rahik-Sikder <96138506+Rahik-Sikder@users.noreply.github.com>
2023-05-21 22:54:05 -0500

    Basic Calendar On Page

--- frontend/healm-app/src/pages/main_pages/Calender_Page.tsx
+++ frontend/healm-app/src/pages/main_pages/Calender_Page.tsx
@@ -1,8 +1,11 @@
-import { Text, View, Image, Pressable } from "react-native";
+import { Text, View, Image, Pressable, TextStyle } from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from "../../styles/styles";
-import { Calendar } from "react-native-calendars";
+import { CalendarList } from "react-native-calendars";
+
+
+
 
 const curDate: Date = new Date();
 
@@ -26,27 +29,73 @@ const curDateString: string =
 interface CalendarPage {}
 
 const CalendarPage: React.FC<CalendarPage> = () => {
+  const [calendarKey, setCalendarKey] = React.useState(0);
+
+  // This Calendar component won't update it's style unless you change the key???
+  React.useEffect(() => {
+    setCalendarKey(Math.random());
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.header_text}>{"Calendar Page"}</Text>
-      <Text style={styles.subheader_text}>
-        {"Current Date: "} {curDateString}
-      </Text>
+      <View>
+        <Text style={styles.header_text}>{"Calendar Page"}</Text>
+        <Text style={styles.subheader_text}>
+          {"Current Date: "} {curDateString}
+        </Text>
+      </View>
+      <View style={styles.calendar_wrapper}>
+        <CalendarList
+          key={calendarKey}
+          horizontal={true}
+          pagingEnabled={true}
 
-      <Calendar
-        key={0} // For not the only way to get the theme to actually apply
-        // Specify style for calendar container element. Default = {}
-        
-        // Specify theme properties to override specific styles for calendar parts. Default = {}
-        theme={{
-          backgroundColor: "#F3F0F7",
-          calendarBackground: "#F3F0F7",
-          textDayFontSize: 16,
-          textDayHeaderFontSize: 16,
-        }}
-      />
+          firstDay={1}
+
+          renderHeader={renderCustomHeader}
+
+          theme={calendarTheme}
+        />
+      </View>
     </View>
   );
 };
 
+function renderCustomHeader(date: any) {
+  const header = date.toString('MMMM yyyy');
+  const [month, year] = header.split(' ');
+  const textStyle: TextStyle = {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingTop: 10,
+    paddingBottom: 10,
+    color: "#F6AF71",
+    paddingRight: 5
+  };
+
+  return (
+    <View style={styles.calendar_header}>
+      <Text style={textStyle}>{`${month} ${year}`}</Text>
+    </View>
+  );
+}
+
+const calendarTheme = {
+  backgroundColor: "#F3F0F7",
+  calendarBackground: "#F3F0F7",
+  todayTextColor: "#F6AF71",
+  textDayFontSize: 20,
+}
+
+
 export default CalendarPage;
+
+// theme={{
+//   backgroundColor: "#F3F0F7",
+//   calendarBackground: "#F3F0F7",
+//   textDayFontSize: 16,
+//   textDayHeaderFontSize: 16,
+//   monthTextColor: "black",
+//   indicatorColor: "black",
+//   arrowColor: 'blue',
+// }}
--- frontend/healm-app/src/styles/styles.tsx
+++ frontend/healm-app/src/styles/styles.tsx
@@ -1,151 +1,164 @@
-import {Dimensions, StyleSheet} from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F3F0F7',
-        alignItems: "center",
-    },
-    image: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height / 2
-    },
-    header_text: {
-        paddingTop: 40,
-        fontSize: 35,
-        fontWeight: "400",
-    },
-    body_text: {
-        paddingTop: 20,
-        paddingHorizontal: 25,
-        fontSize: 18,
-        textAlign: "center",
-        fontWeight: "normal",
-    },
-    icon_arrow:{
-        alignItems: "flex-end"
-    },
-    icon_progress:{
-        alignItems: "flex-start",
-        flexDirection: "row",
-        flex: 1,
-    },
-    icon_container:{
-        position: "absolute",
-        bottom: 100,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        paddingHorizontal: 45,
-    },
-    Bottom_Text_Container:{
-        position: "absolute",
-        bottom: 50,
-        justifyContent: "space-between"
-    },
-    text_input_box: {
-        backgroundColor: 'white',
-        width: Dimensions.get("screen").width*0.8,
-        height: 60,
-        marginTop: 15,
-        borderRadius: 50,
-        flexDirection: "row",
-    },
-    text_input_box_with_error: {
-        borderColor: 'red',
-        borderWidth: 2,
-        backgroundColor: 'white',
-        width: Dimensions.get("screen").width*0.8,
-        height: 60,
-        marginTop: 15,
-        borderRadius: 15,
-        flexDirection: "row",
-    },
-    text_input_text:{
-        color: 'black',
-        fontSize: 16,
-        fontWeight: "normal",
-        alignSelf: "center",
-        margin: 10,
-        flexDirection: "row",
-        overflow: "hidden",
-        maxWidth: 250,
-        width: 250,
-    },
-    text_input_icons: {
-        alignSelf: "center",
-        marginLeft: 15,
-        marginRight: 10
-    },
-    login_button: {
-        width: Dimensions.get("screen").width*0.8,
-        height: 60,
-        marginTop: 25,
-        borderRadius: 50,
-        backgroundColor: "#FFDCB2",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    login_with_app_button: {
-        width: Dimensions.get("screen").width*0.8,
-        height: 55,
-        marginBottom: 20,
-        borderRadius: 50,
-        backgroundColor: "#ffffff",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-    },
-    login_button_text: {
-        color: 'black',
-        fontWeight: "500",
-        fontSize: 18,
-    },
-    new_user_signup_button: {
-        alignSelf: "center",
-        marginBottom: 15,
-        flexDirection: "row",
-    },
-    new_user_signup_button_text: {
-        color: '#F6AF71',
-        fontWeight: "normal",
-        fontSize: 16,
-    },
-    subheader_text: {
-        color: 'black',
-        fontWeight: "normal",
-        fontSize: 16,
-    }, page_header_text: {
-        paddingTop: 40,
-        fontSize: 35,
-        fontWeight: "bold",
-        alignSelf: "flex-start"
-    },
-    page_subheader_text: {
-        paddingTop: 10,
-        fontSize: 20,
-        fontWeight: "300",
-        alignSelf: "flex-start",
-        paddingBottom: 20
-    },
-    divider: {
-        marginVertical: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    line: {
-        height: 1,
-        width: Dimensions.get("screen").width*0.25,
-        backgroundColor: '#E2E8F0',
-    },
-    dividerText:{
-        marginHorizontal: 10,
-        fontSize: 16,
-        fontWeight: '300',
-        color: "#808D9E"
-    },
-    elementContainer:{
-        marginVertical: 0,
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#F3F0F7",
+    alignItems: "center",
+  },
+  image: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height / 2,
+  },
+  header_text: {
+    paddingTop: 40,
+    fontSize: 35,
+    fontWeight: "400",
+  },
+  body_text: {
+    paddingTop: 20,
+    paddingHorizontal: 25,
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "normal",
+  },
+  icon_arrow: {
+    alignItems: "flex-end",
+  },
+  icon_progress: {
+    alignItems: "flex-start",
+    flexDirection: "row",
+    flex: 1,
+  },
+  icon_container: {
+    position: "absolute",
+    bottom: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 45,
+  },
+  Bottom_Text_Container: {
+    position: "absolute",
+    bottom: 50,
+    justifyContent: "space-between",
+  },
+  text_input_box: {
+    backgroundColor: "white",
+    width: Dimensions.get("screen").width * 0.8,
+    height: 60,
+    marginTop: 15,
+    borderRadius: 50,
+    flexDirection: "row",
+  },
+  text_input_box_with_error: {
+    borderColor: "red",
+    borderWidth: 2,
+    backgroundColor: "white",
+    width: Dimensions.get("screen").width * 0.8,
+    height: 60,
+    marginTop: 15,
+    borderRadius: 15,
+    flexDirection: "row",
+  },
+  text_input_text: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "normal",
+    alignSelf: "center",
+    margin: 10,
+    flexDirection: "row",
+    overflow: "hidden",
+    maxWidth: 250,
+    width: 250,
+  },
+  text_input_icons: {
+    alignSelf: "center",
+    marginLeft: 15,
+    marginRight: 10,
+  },
+  login_button: {
+    width: Dimensions.get("screen").width * 0.8,
+    height: 60,
+    marginTop: 25,
+    borderRadius: 50,
+    backgroundColor: "#FFDCB2",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  login_with_app_button: {
+    width: Dimensions.get("screen").width * 0.8,
+    height: 55,
+    marginBottom: 20,
+    borderRadius: 50,
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  login_button_text: {
+    color: "black",
+    fontWeight: "500",
+    fontSize: 18,
+  },
+  new_user_signup_button: {
+    alignSelf: "center",
+    marginBottom: 15,
+    flexDirection: "row",
+  },
+  new_user_signup_button_text: {
+    color: "#F6AF71",
+    fontWeight: "normal",
+    fontSize: 16,
+  },
+  subheader_text: {
+    color: "black",
+    fontWeight: "normal",
+    fontSize: 16,
+  },
+  page_header_text: {
+    paddingTop: 40,
+    fontSize: 35,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+  },
+  page_subheader_text: {
+    paddingTop: 10,
+    fontSize: 20,
+    fontWeight: "300",
+    alignSelf: "flex-start",
+    paddingBottom: 20,
+  },
+  divider: {
+    marginVertical: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  line: {
+    height: 1,
+    width: Dimensions.get("screen").width * 0.25,
+    backgroundColor: "#E2E8F0",
+  },
+  dividerText: {
+    marginHorizontal: 10,
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#808D9E",
+  },
+  elementContainer: {
+    marginVertical: 0,
+  },
+  calendar_wrapper: {
+    height: 400,
+    position: "absolute",
+    bottom: 0,
+  },
+  calendar_header: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "flex-start",
+    marginTop: 10,
+    marginBottom: 10,
+  },
 
 });
